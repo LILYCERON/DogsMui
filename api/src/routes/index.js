da8c0b8ce75760238e765a57d63b0d7fe0fdec6b
Liliana <lilyceron00@gmail.com>
2024-02-12 16:55:58 -0500

    routePost

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,19 +11,24 @@ const router = Router();
 router.get('/dogs', async (req, res) => {
 
     const breed = req.query.name
+    console.log(breed)
+    console.log(isNaN(breed))
     const dogsApi = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json())
 
-    if (breed !== undefined && breed !== '') {
-        const breedSearched = dogsApi.filter((bre) => bre.name.toLocaleLowerCase().includes(breed.toLocaleLowerCase()))
-        if (breedSearched.length > 0) {
+    if (breed === undefined) {
+        res.send(dogsApi)
 
+    } else if (breed !== "") {
+
+        const breedSearched = dogsApi.filter((bre) => bre.name.toLocaleLowerCase().includes(breed.toLocaleLowerCase()))
+
+        if (breedSearched.length > 0) {
             res.send(breedSearched)
         } else {
             res.send(`there is not result for the breed ${breed}`)
         }
-
     } else {
-        res.send('your search must contain alphabetic characters')
+        res.send('enter a breed of your interest')
     }
 })
 
@@ -60,4 +65,18 @@ router.get('/temperaments', async (req, res) => {
     res.send(cleanTemperament)
 
 })
+
+router.post('/dogs', (req, res) => {
+
+    const { name, id, image, breed, temperament, height, weight } = req.body
+
+    if (name.length > 0) {
+
+        res.send(name, image, temperament)
+    } else {
+        res.send('enter a valid name ')
+    }
+
+})
+
 module.exports = router;
