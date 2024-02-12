44ea3aec6bed8fd4f1bd1eae4917ec9270c48348
Liliana <lilyceron00@gmail.com>
2024-02-12 14:05:31 -0500

    Backend Dogs

---
+++ api/src/routes/index.js
@@ -0,0 +1,63 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.get('/dogs', async (req, res) => {
+
+    const breed = req.query.name
+    const dogsApi = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json())
+
+    if (breed !== undefined && breed !== '') {
+        const breedSearched = dogsApi.filter((bre) => bre.name.toLocaleLowerCase().includes(breed.toLocaleLowerCase()))
+        if (breedSearched.length > 0) {
+
+            res.send(breedSearched)
+        } else {
+            res.send(`there is not result for the breed ${breed}`)
+        }
+
+    } else {
+        res.send('your search must contain alphabetic characters')
+    }
+})
+
+router.get('/dogs/:id', async (req, res) => {
+
+    const id = Number(req.params.id)
+    const dogsApi = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json())
+
+    if (Number(id) && id !== undefined) {
+        const breedIs = dogsApi.filter((bre) => bre.id === id)
+        if (Object.keys(breedIs).length >= 1) {
+            res.send(breedIs)
+        } else {
+            res.send(`There is not a breed whit id: ${id}`)
+        }
+    } else {
+        res.send(`checkes that the data entered is a number `)
+    }
+})
+
+router.get('/temperaments', async (req, res) => {
+
+    const dogsApi = await fetch('https://api.thedogapi.com/v1/breeds').then(data => data.json())
+
+    const setTemperaments = dogsApi.map((bre) => bre.temperament)
+
+    const joinTemperaments = setTemperaments.join(", ")
+
+    const splitTemperaments = joinTemperaments.split(", ")
+
+    const cleanTemperament = splitTemperaments.filter((temperament, index) => splitTemperaments.indexOf(temperament) === index)
+
+
+    res.send(cleanTemperament)
+
+})
+module.exports = router;
